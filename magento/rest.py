d2aaa2aced24d3ff9c4d82673270113206378fdc
Stefan Rijnhart <stefan@opener.am>
2017-04-17 13:55:07 +0200

    [IMP] Raise with actual Magento error as message

--- magento/rest.py
+++ magento/rest.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 try:
     import requests
+    from requests import HTTPError
     import json
 except ImportError:
     pass
@@ -26,8 +27,11 @@ class Client(object):
             kwargs['data'] = json.dumps(arguments)
             headers['Content-Type'] = 'application/json'
         res = function(url, **kwargs)
-        res.raise_for_status()
+        if (res.status_code == 400 and res._content):
+            raise HTTPError(
+                url, res.status_code, res._content, headers, __name__)
         json_content = res.json()
+        res.raise_for_status()
         if isinstance(json_content, unicode):
             # Wyomind API responses are double json encoded
             json_content = json.loads(json_content)
